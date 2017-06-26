f9fe719e84ce9c269474dee61b66045a27077867
david krueger <dk12181@gmail.com>
2017-06-26 14:55:08 -0500

    fianl

--- client.js
+++ client.js
@@ -6,16 +6,16 @@ var counter = 1;
 
   $('#generateButton').on('click', function(){
     //console.log('button clicked');
-    $('.container').append("<div data-class='div'>" + "<p>" + counter + "</p>" + "<button id='swapNow'>Swap</button>" + "<button id='deleteNow'>Delete</button>" + "</div>");
+    $('.container').append("<div class='div'>" + "<p>" + counter + "</p>" + "<button id='swapNow'>Swap</button>" + "<button id='deleteNow'>Delete</button>" + "</div>");
     counter += 1;
   });
 
   $('#swapNow').on('click', function(){
-    $('<div>').addClass("<div data-class='newDiv'></div>");
+    $('.container div').swap("<div class='newDiv'></div>");
   });
 
   $('#deleteNow').on('click', function(){
-    $('<div>').remove();
+    $('.contaner div').remove();
   });
 
 });
